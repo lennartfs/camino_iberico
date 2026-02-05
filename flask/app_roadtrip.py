96915758f09d411f3df4728ee84539eb66abb527
lennartfs <lennartfreriks@hotmail.com>
2026-02-05 19:16:09 +0100

    Update app_roadtrip.py

--- flask/app_roadtrip.py
+++ flask/app_roadtrip.py
@@ -49,33 +49,15 @@ def uren(getal):
 
 @app.route("/review_get", methods=["GET"])
 def reviewget():
-    conn = psycopg2.connect(**DB_CONFIG)
+    print("ROUTE START")
+    conn = psycopg2.connect(connect_timeout=5, **DB_CONFIG)
+    print("DB CONNECTED")
+
     cur = conn.cursor()
-    cur.execute(
-        """
-        SELECT CONCAT(g.voornaam, ' ', g.naam) AS gebruiker, r.beoordeling, concat(t.start_stad, '-', t.eind_stad) as route, r.beschrijvring
-        from project_roadtrip.gebruikers g
-        inner join
-            project_roadtrip.recensies r on g.geb_id = r.geb_id
-        inner join
-            project_roadtrip.roadtrips t on r.route_id = t.route_id
-        order by r.rec_id desc
-        """,
-    )
-    tabel_tup = cur.fetchall()
-    tabel_dict = [
-        {
-            "gebruiker": row[0],
-            "beoordeling": int(row[1]),
-            "route": row[2],
-            "beschrijving": row[3],
-        }
-        for row in tabel_tup
-    ]
-    tabel_json = jsonify({"reviews": tabel_dict})
-    cur.close()
-    conn.close()
-    return tabel_json
+    cur.execute("SELECT 1")
+    print("QUERY OK")
+
+    return {"status": "done"}
 
 
 @app.route("/opvragen_autos", methods=["GET"])
@@ -395,4 +377,5 @@ def send_mail():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
 
